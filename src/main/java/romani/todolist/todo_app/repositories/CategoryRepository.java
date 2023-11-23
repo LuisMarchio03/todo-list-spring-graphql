d79308a949b7200b8defc265343018547254c917
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2023-11-23 11:01:21 -0300

    feat: create CategoryRepository

---
+++ src/main/java/romani/todolist/todo_app/repositories/CategoryRepository.java
@@ -0,0 +1,20 @@
+package romani.todolist.todo_app.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import romani.todolist.todo_app.entities.Category;
+
+// @Repository: anotação que indica que a classe é um repositório
+// Repositório: classe que permite realizar operações no banco de dados
+// CategoryRepository: nome do repositório
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, String> {
+  // findAllCategoriesByUserId: método que permite buscar todas as categorias de
+  // um
+  // usuário
+  // Iterable<Category>: tipo de retorno do método
+  // findAllCategoriesByUserId: nome do método
+  // String userId: parâmetro do método
+  Iterable<Category> findAllCategoriesByUserId(String userId);
+
+}
